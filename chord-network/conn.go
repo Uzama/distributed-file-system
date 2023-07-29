1229a3000bbf7740505bd5632acfdc6237c5e7f4
Uzama Zaid <uzama.jaward@system73.com>
2023-07-29 15:47:30 +0530

    implemented method for make remote connection

--- chord-network/conn.go
+++ chord-network/conn.go
@@ -21,3 +21,31 @@ func Dial(ip string, options ...grpc.DialOption) (*grpc.ClientConn, error) {
 
 	return grpc.DialContext(ctx, ip, options...)
 }
+
+func (c *Chord) connectToRemote(remoteIP string) (proto.CommunicationClient, error) {
+	c.connLock.RLock()
+	defer c.connLock.RUnlock()
+
+	grpc, ok := c.connectionsPool[remoteIP]
+	if ok {
+		c.connLock.RUnlock()
+		return grpc.Client, nil
+	}
+
+	c.logger.Printf("connect-to-temote: dialing to %s\n", remoteIP)
+
+	conn, err := Dial(remoteIP, c.config.DialOptions...)
+	if err != nil {
+		c.logger.Printf("connect-to-temote: dialing failed to %s\n", err.Error())
+		return nil, err
+	}
+
+	c.logger.Println("connect-to-temote: dialed success")
+
+	client := proto.NewCommunicationClient(conn)
+	grpc = &GRPCConn{remoteIP, client, conn}
+
+	c.connectionsPool[remoteIP] = grpc
+
+	return client, nil
+}

4539dc19534df4bdf1ff287571530999f48785e4
Uzama Zaid <uzama.jaward@system73.com>
2023-07-29 21:29:11 +0530

    added channels to listen for graceful shurdown

--- chord-network/main.go
+++ chord-network/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -20,14 +24,26 @@ func main() {
 
 	cfg := defaultConfig()
 
-	h, err := newChord(cfg, "localhost:8001", "")
+	h, err := newChord(cfg, "localhost:8004", "localhost:8001")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	for {
-		time.Sleep(5 * time.Second)
-		fmt.Println(h.Print())
-	}
+	go func() { 
+		for {
+			time.Sleep(5 * time.Second)
+			fmt.Println(h.Print())
+		}
+	} ()
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
+
+	<-c
+
+	h.leave(context.Background())
+
+	os.Exit(0)
 
 }

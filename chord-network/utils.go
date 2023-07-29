cd244522caec50baafe69c921b690b46fa139652
weegaya <gayamweerasinghe@gmail.com>
2023-07-29 18:00:30 +0530

    Generate random IPs

--- chord-network/utils.go
+++ chord-network/utils.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 // to enable Debug, set this to true
@@ -27,4 +30,25 @@ func logAndExitOnError(source string, err error) {
 	if err != nil {
 		log.Fatal(source+": ", err)
 	}
+}
+
+// Generate random IPs
+func generateUniqueIPs(baseIP string, count int) []string {
+	source := rand.NewSource(time.Now().UnixNano())
+	randomizer := rand.New(source)
+
+	uniqueIPs := make(map[string]bool)
+	generatedIPs := make([]string, count)
+
+	for i := 0; i < count; {
+		newIP := baseIP + ":" + strconv.Itoa(randomizer.Intn(60000)+1535)
+		_, exists := uniqueIPs[newIP]
+		if !exists {
+			uniqueIPs[newIP] = true
+			generatedIPs[i] = newIP
+			i++
+		}
+	}
+
+	return generatedIPs
 }
